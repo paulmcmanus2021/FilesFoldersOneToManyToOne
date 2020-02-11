c5ac30d5bc5906498133086051d587b68860ec70
Paul McManus <pmcmanus2021@gmail.com>
2020-02-11 17:03:20 +0000

    Adds File/Folder/User classes

--- src/main/java/com/example/codeclan/FilesFoldersOneToManyToOne/models/Folder.java
+++ src/main/java/com/example/codeclan/FilesFoldersOneToManyToOne/models/Folder.java
@@ -1,4 +1,68 @@
 package com.example.codeclan.FilesFoldersOneToManyToOne.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "folders")
 public class Folder {
+
+    @Column(name = "title")
+    private String title;
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "folder")
+    private List<File> files;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Folder(String title, User user) {
+        this.title = title;
+        this.user = user;
+        this.files = new ArrayList<>();
+    }
+
+    public Folder() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
