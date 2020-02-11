92fd5fb354d4b57598f33039d891e55a8a4c65ec
Paul McManus <pmcmanus2021@gmail.com>
2020-02-11 20:26:46 +0000

    Controllers tested

--- src/main/java/com/example/codeclan/FilesFoldersOneToManyToOne/controllers/FolderController.java
+++ src/main/java/com/example/codeclan/FilesFoldersOneToManyToOne/controllers/FolderController.java
@@ -1,4 +1,34 @@
 package com.example.codeclan.FilesFoldersOneToManyToOne.controllers;
 
+import com.example.codeclan.FilesFoldersOneToManyToOne.models.Folder;
+import com.example.codeclan.FilesFoldersOneToManyToOne.repositories.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class FolderController {
+
+    @Autowired
+    private FolderRepository folderRepository;
+
+    @GetMapping(value = "/folders")
+    public ResponseEntity<List<Folder>> getAllFolders() {
+        return new ResponseEntity<>(folderRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/folders/{id}")
+    public ResponseEntity getFolder(@PathVariable Long id) {
+        return new ResponseEntity<>(folderRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "folders")
+    public ResponseEntity<Folder> createFolder(@RequestBody Folder folder){
+        folderRepository.save(folder);
+        return new ResponseEntity<>(folder, HttpStatus.CREATED);
+    }
+
 }
